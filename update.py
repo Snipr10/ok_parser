1175d3647732e75cdf5f225a8a17086169ca3c6e
oleg <gusevoleg@gmail.com>
2022-05-03 14:29:07 +0300

    fix

--- update.py
+++ update.py
@@ -41,54 +41,80 @@ if __name__ == '__main__':
 
     owners = list(Owner.objects.all())
     i = 0
-    while len(owners) > 0:
+    owners_delete = []
+    for o in owners:
         i += 1
         print(i)
-        owner_id = []
-        owner_new = []
-
-        owner = owners.pop()
-        owner_id.append(owner)
-        for o in owners:
-            if o.screen_name == owner.screen_name:
-                owner_id.append(o)
-            else:
-                for z in owners:
-                    if z.screen_name == owner.screen_name and z.id !=owner.id :
-                        owner_new.append(o)
-                        break
-        owners = owner_new
-
-        id_ = owner.sphinx_id
-        real_owner = None
-        for ow in owner_id:
-            if ow.id == id_:
-                real_owner = ow
-                break
-        if not real_owner:
-            try:
-                real_owner = Owner(
-                            id=id_,
-                            screen_name=owner.screen_name,
-                            username=owner.username,
-                            name=owner.name,
-                            avatar=owner.avatar,
-                            sphinx_id=id_
-                    )
-            except Exception:
-                continue
-
-        for o_id in owner_id:
-            for p in Posts.objects.filter(owner_id=o_id.id):
-                p.owner_id=real_owner.id
+        if o.sphinx_id != o.id:
+            for p in Posts.objects.filter(owner_id=o.id):
+                p.owner_id=o.sphinx_id
                 p.save()
-            for p in Posts.objects.filter(from_id=o_id.id):
-                p.from_id=real_owner.id
+            for p in Posts.objects.filter(from_id=o.id):
+                p.from_id= o.sphinx_id
                 p.save()
-
-        for o_id in owner_id:
-            if o_id.id != real_owner.id:
-                o_id.delete()
+        try:
+            Owner(
+                id=o.sphinx_id,
+                screen_name=o.screen_name,
+                username=o.username,
+                name=o.name,
+                avatar=o.avatar,
+                sphinx_id=o.sphinx_id
+            )
+        except Exception as e:
+            print(e)
+        try:
+            o.delete()
+        except Exception as e:
+            print(e)
+    # while len(owners) > 0:
+    #     i += 1
+    #     print(i)
+    #     owner_id = []
+    #     owner_new = []
+    #
+    #     owner = owners.pop()
+    #     owner_id.append(owner)
+    #     for o in owners:
+    #         if o.screen_name == owner.screen_name:
+    #             owner_id.append(o)
+    #         else:
+    #             for z in owners:
+    #                 if z.screen_name == owner.screen_name and z.id !=owner.id :
+    #                     owner_new.append(o)
+    #                     break
+    #     owners = owner_new
+    #
+    #     id_ = owner.sphinx_id
+    #     real_owner = None
+    #     for ow in owner_id:
+    #         if ow.id == id_:
+    #             real_owner = ow
+    #             break
+    #     if not real_owner:
+    #         try:
+    #             real_owner = Owner(
+    #                         id=id_,
+    #                         screen_name=owner.screen_name,
+    #                         username=owner.username,
+    #                         name=owner.name,
+    #                         avatar=owner.avatar,
+    #                         sphinx_id=id_
+    #                 )
+    #         except Exception:
+    #             continue
+    #
+    #     for o_id in owner_id:
+    #         for p in Posts.objects.filter(owner_id=o_id.id):
+    #             p.owner_id=real_owner.id
+    #             p.save()
+    #         for p in Posts.objects.filter(from_id=o_id.id):
+    #             p.from_id=real_owner.id
+    #             p.save()
+    #
+    #     for o_id in owner_id:
+    #         if o_id.id != real_owner.id:
+    #             o_id.delete()
         # is_bad_screen_name = False
         # try:
         #     int(owner.screen_name)

255bff9099c885f147bb161aa2548745fed71502
oleg.gusev <gusevoleg@gmail.com>
2022-07-07 08:41:24 +0300

    update user

--- screen_name.py
+++ screen_name.py
@@ -29,7 +29,12 @@ if __name__ == '__main__':
     import datetime
     from core.models import Posts, Sessions, Keyword, Sources, Owner
     for u in Owner.objects.filter(screen_name__isnull=True, username__isnull=False):
-        s = requests.get(f"https://ok.ru/{u.username}")
-        screen_name=BeautifulSoup(s.text).find("link", {"rel":"alternate"}).get("href").split("/")[-1]
-        u.screen_name=screen_name
-        u.save(update_fields=['screen_name'])
+        try:
+            print(u.username)
+            s = requests.get(f"https://ok.ru/{u.username}")
+            screen_name=BeautifulSoup(s.text).find("link", {"rel":"alternate"}).get("href").split("/")[-1]
+            u.screen_name=screen_name
+            u.save(update_fields=['screen_name'])
+            print("save")
+        except Exception as e:
+            print(f"cant get {e}")

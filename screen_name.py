116e3f0c4014137e85227fce50e6e3287553dded
oleg.gusev <gusevoleg@gmail.com>
2022-07-07 18:27:21 +0300

    update user

--- screen_name.py
+++ screen_name.py
@@ -93,6 +93,11 @@ if __name__ == '__main__':
     #         print("save")
     #     except Exception as e:
     #         print(f"cant get")
+    for u in Owner.objects.filter(screen_name__isnull=True):
+        u_id = u.id
+
+        if len(Posts.objects.filter(from_id=u_id)) == 0 and len(Posts.objects.filter(owner_id=u_id)) :
+            u.delete()
 
     for u in Owner.objects.filter(screen_name__isnull=True):
         try:

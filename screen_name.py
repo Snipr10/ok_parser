b531459857e2bea4f1d4bda22dc02e78839f72f6
oleg.gusev <gusevoleg@gmail.com>
2022-07-07 08:36:43 +0300

    update user

---
+++ screen_name.py
@@ -0,0 +1,35 @@
+import os
+
+import requests
+from bs4 import BeautifulSoup
+
+if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ok_parser.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    print(1)
+    import django
+
+    django.setup()
+    import pymysql
+
+    pymysql.install_as_MySQLdb()
+
+    from parse_group import get_all_group_post
+    from parse_profile import get_all_profile_post
+    from search import get_all_posts
+    from accounts import update_time_timezone
+    from django.utils import timezone
+    import datetime
+    from core.models import Posts, Sessions, Keyword, Sources, Owner
+    for u in Owner.objects.filter(screen_name__isnull=True, username__isnull=False):
+        s = requests.get(f"https://ok.ru/{u.username}")
+        screen_name=BeautifulSoup(s.text).find("link", {"rel":"alternate"}).get("href").split("/")[-1]
+        u.screen_name=screen_name
+        u.save(update_fields=['screen_name'])

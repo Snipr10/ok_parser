530ed168f8bd745a50c6efc679762f8b3fb5ccd5
oleg.gusev <gusevoleg@gmail.com>
2022-11-03 10:17:52 +0300

    added followers

--- login.py
+++ login.py
@@ -26,29 +26,30 @@ def login(session, login_, password_, session_data=None, attempt=0):
         }
         session.proxies.update(proxies)
         print(proxies)
-        proxy_2_cap = {'type': 'HTTP', 'uri': f'{session_proxy.login}:{session_proxy.proxy_password}@{session_proxy.ip}:{session_proxy.port}'}
+        proxy_2_cap = {'type': 'HTTP',
+                       'uri': f'{session_proxy.login}:{session_proxy.proxy_password}@{session_proxy.ip}:{session_proxy.port}'}
     except Exception as e:
         proxy_2_cap = None
         print(f"Proxy error : {e}")
     ""
     login_headers = {
         'authority': 'ok.ru',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'accept-language': 'ru-RU,ru;q=0.9',
         'cache-control': 'max-age=0',
-        'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"',
+        'content-type': 'application/x-www-form-urlencoded',
+        'dnt': '1',
+        'origin': 'https://ok.ru',
+        'referer': 'https://ok.ru/dk?st.cmd=anonymMain&st.layer.cmd=PopLayerClose',
+        'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100"',
         'sec-ch-ua-mobile': '?0',
         'sec-ch-ua-platform': '"Linux"',
-        'origin': 'https://ok.ru',
-        'upgrade-insecure-requests': '1',
-        'dnt': '1',
-        'content-type': 'application/x-www-form-urlencoded',
-        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36',
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'sec-fetch-site': 'same-origin',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-user': '?1',
         'sec-fetch-dest': 'document',
-        'referer': 'https://ok.ru/',
-        'accept-language': 'ru-RU,ru;q=0.9',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36'
     }
     url = 'https://ok.ru/dk?cmd=AnonymLogin&st.cmd=anonymLogin'
 
@@ -57,11 +58,12 @@ def login(session, login_, password_, session_data=None, attempt=0):
               '&st.posted=set' \
               '&st.fJS=on' \
               '&st.st.screenSize=1920%2Bx%2B1080' \
-              '&st.st.browserSize=980' \
+              '&st.st.browserSize=948' \
               '&st.st.flashVer=0.0.0' \
               f'&st.email={login_}' \
               f'&st.password={password_}' \
               '&st.iscode=false'
+
     res = session.post(url, headers=login_headers, data=payload)
     if "topPanelLeftCorner" not in res.text or "TD_Logout" not in res.text:
         if "st.cmd=anonymUnblockConfirmPhone" in res.text:
@@ -78,7 +80,7 @@ def login(session, login_, password_, session_data=None, attempt=0):
         attempt += 1
         res_cap = session.get("https://ok.ru/captcha?st.cmd=captcha")
         text = res_cap.content
-        file_name = f"{login_}{random.randint(0,100)}{random.randint(0,100)}{random.randint(0,100)}{random.randint(0,100)}.jpg"
+        file_name = f"{login_}{random.randint(0, 100)}{random.randint(0, 100)}{random.randint(0, 100)}{random.randint(0, 100)}.jpg"
         fp = open(file_name, 'wb')
         fp.write(text)
         fp.close()

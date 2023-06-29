3cede30e4be884da12fc180f6681af79e90aa37f
oleg.gusev <oleg.gusev@gmail.com>
2023-06-29 09:43:50 +0300

    fixed

--- accounts.py
+++ accounts.py
@@ -11,9 +11,12 @@ def update_time_timezone(my_time):
 
 
 def get_new_session():
-    return Sessions.objects.filter(Q(last_parsing__isnull=True) | Q(last_parsing__lte=update_time_timezone(
+    s = Sessions.objects.filter(Q(last_parsing__isnull=True) | Q(last_parsing__lte=update_time_timezone(
         timezone.localtime()) - timedelta(minutes=5)), is_parsing=False, is_active__lte=100, proxy_id__isnull=False
                                    ).order_by('last_parsing').first()
+    s.is_parsing=True
+    s.save(update_fields=["is_parsing"])
+    return s
 
 
 def get_session_update(session):

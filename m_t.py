877bbdc732ec4ea0822168e29851e8a5652880b9
oleg.gusev <oleg.gusev@gmail.com>
2024-02-08 18:29:10 +0300

    deploy

--- m_t.py
+++ m_t.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 
+import multiprocessing
 import os
+import random
 import re
+import threading
 import time
 
 import django.db
-import threading
-import random
-import multiprocessing
-
 import requests
 from bs4 import BeautifulSoup
 from django.db.models import Q
-
 
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ok_parser.settings')
@@ -33,17 +31,28 @@ if __name__ == '__main__':
 
     pymysql.install_as_MySQLdb()
 
+    import datetime
+
+    from django.utils import timezone
+
+    from accounts import update_time_timezone
+    from core.models import (AllProxy, BannedProxy, Keyword, Owner, Posts,
+                             Sessions, Sources, SourcesItems)
+    from login import get_new_proxy
     from parse_group import get_all_group_post
     from parse_profile import get_all_profile_post
     from search import get_all_posts
     i =0
 
     for p in Posts.objects.filter():
-        i += 1
-        print(i)
-        if "https://" not in p.url:
-          p.url = "https://ok.ru" + p.url.replace("%2F", "/")
-          p.save()
-        elif "ok.ruvideo" in p.url:
-            p.url = p.url.replace("ok.ruvideo", "ok.ru/video")
-            p.save()
+        try:
+            i += 1
+            print(i)
+            if "https://" not in p.url:
+              p.url = "https://ok.ru" + p.url.replace("%2F", "/")
+              p.save()
+            elif "ok.ruvideo" in p.url:
+                p.url = p.url.replace("ok.ruvideo", "ok.ru/video")
+                p.save()
+        except Exception:
+            pass
